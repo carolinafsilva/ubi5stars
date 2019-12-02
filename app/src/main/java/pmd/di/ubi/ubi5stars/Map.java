beae7d18e562edc6080d254cd869726a3511dd3a
João Fraga <joaoluisfreirefraga@gmail.com>
2019-12-02 12:53:41 +0000

    Add some layout

---
+++ app/src/main/java/pmd/di/ubi/ubi5stars/Map.java
@@ -0,0 +1,23 @@
+package pmd.di.ubi.ubi5stars;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Map extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    public void showMenu(View v) {
+        startActivity(new Intent(this, Menu.class));
+    }
+
+    public void search(View v) {
+        // TODO: implement
+    }
+}
