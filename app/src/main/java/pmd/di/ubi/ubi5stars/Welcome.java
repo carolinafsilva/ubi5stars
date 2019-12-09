e414a752c98de7c9e388d2c765b1ad30bb5a8334
Dayveed117 <david.bugalho@ubi.pt>
2019-12-09 11:45:59 +0000

    priti
    Welcome screen fixolas

---
+++ app/src/main/java/pmd/di/ubi/ubi5stars/Welcome.java
@@ -0,0 +1,28 @@
+package pmd.di.ubi.ubi5stars;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Welcome extends Activity {
+
+    private final int secs = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Welcome.this, Login.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(i);
+                finish();
+            }
+        }, secs);
+    }
+}
