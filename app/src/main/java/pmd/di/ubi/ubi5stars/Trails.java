a761652c1d47874d77ddd2dba9beeac7e3c3c5fc
Dayveed117 <david.bugalho@ubi.pt>
2019-12-16 01:29:56 +0000

    Layouts
    Rough styling dos percursos e definições nem tanto

---
+++ app/src/main/java/pmd/di/ubi/ubi5stars/Trails.java
@@ -0,0 +1,32 @@
+package pmd.di.ubi.ubi5stars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Trails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trails);
+    }
+
+    // Ou apenas fazer um método e depois faz-se um switch dependendo do ID da view
+
+    public void getCurrCoord(View view) {
+    }
+
+    public void museumTrail(View view) {
+    }
+
+    public void urbanartTrail(View view) {
+    }
+
+    public void gardenTrail(View view) {
+    }
+
+    public void nightlifeTrail(View view) {
+    }
+}
