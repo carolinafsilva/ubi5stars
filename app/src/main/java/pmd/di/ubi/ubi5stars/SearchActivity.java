1bbc634a64931f47eb9ce472790f6f40436e15dd
Carolina Silva <carolsilva.acfs@gmail.com>
2019-12-19 02:44:44 +0000

    YAY, a pesquisa já funciona

--- app/src/main/java/pmd/di/ubi/ubi5stars/SearchActivity.java
+++ app/src/main/java/pmd/di/ubi/ubi5stars/SearchActivity.java
@@ -1,39 +1,71 @@
 package pmd.di.ubi.ubi5stars;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
-public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    ArrayAdapter<String> adapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class SearchActivity extends Activity {
+
+    private final String locationsCollection = "locations";
+
+    private TextView tvSearchBar;
+    private LinearLayout searchResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_search);
 
-        ListView listV=(ListView)findViewById(R.id.list);   // Colocar titulo na listView
-
-        Spinner spinner = findViewById(R.id.spinner1);  // Spinner
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.classes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        tvSearchBar = findViewById(R.id.search_bar);
+        searchResult = findViewById(R.id.search_result);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {   // Implement Query to database and show results
+    public void search(View v) {
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference(locationsCollection);
+
+        final String name = tvSearchBar.getText().toString();
+
+        databaseRef.orderByChild("name").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    LocationCollection l = ds.getValue(LocationCollection.class);
+                    if (l.getName().contains(name)) {
+                        LinearLayout ll = (LinearLayout) getLayoutInflater().inflate(R.layout.search_result, null);
+
+                        TextView tvLocation = ll.findViewById(R.id.location);
+                        ImageView imageView = ll.findViewById(R.id.image_view);
+
+                        tvLocation.setText(l.getName());
+
+                        Picasso.with(SearchActivity.this)
+                                .load(l.getImageURL())
+                                .fit()
+                                .centerCrop()
+                                .into(imageView);
+
+                        searchResult.addView(ll);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(SearchActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
 }
