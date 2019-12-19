7ef500c7263f5ab79aaf08d2913e88004c0543fc
Dayveed117 <david.bugalho@ubi.pt>
2019-12-19 23:59:46 +0000

    últimoO
    Signed-off-by: Dayveed117 <david.bugalho@ubi.pt>

--- app/src/main/java/pmd/di/ubi/ubi5stars/TrailsActivity.java
+++ app/src/main/java/pmd/di/ubi/ubi5stars/TrailsActivity.java
@@ -15,26 +15,26 @@ public class TrailsActivity extends AppCompatActivity {
     }
 
     public void getCurrCoord(View view) {
-        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_SHORT).show();
     }
 
     public void traceTrail(View view) {
-        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_SHORT).show();
     }
 
     public void museumTrail(View view) {
-        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_SHORT).show();
     }
 
     public void urbanartTrail(View view) {
-        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_SHORT).show();
     }
 
     public void gardenTrail(View view) {
-        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_SHORT).show();
     }
 
     public void nightlifeTrail(View view) {
-        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), R.string.coming_soon, Toast.LENGTH_SHORT).show();
     }
 }
