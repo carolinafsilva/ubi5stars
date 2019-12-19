ff84f23bffc4ba7b405b00b406c201982c7c8607
João Fraga <joaoluisfreirefraga@gmail.com>
2019-12-19 19:44:57 +0000

    Code++

--- app/src/main/java/pmd/di/ubi/ubi5stars/UserProfileActivity.java
+++ app/src/main/java/pmd/di/ubi/ubi5stars/UserProfileActivity.java
@@ -3,10 +3,20 @@ package pmd.di.ubi.ubi5stars;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class UserProfileActivity extends AppCompatActivity {
+
+    private ImageView IVphoto;
 
     private EditText ETuser;
     private EditText ETpass;
@@ -20,19 +30,51 @@ public class UserProfileActivity extends AppCompatActivity {
 
     public void uploadFoto(View v) {
 
+        IVphoto = findViewById(R.id.photo);
+        
     }
 
     public void editUserData(View view) {
 
-        ETuser = findViewById(R.id.up_ins_email);
-        ETpass = findViewById(R.id.up_ins_password);
-        ETconf_pass = findViewById(R.id.up_password_confirmation);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        String user = ETuser.getText().toString();
-        String pass = ETpass.getText().toString();
-        String conf_pass = ETconf_pass.getText().toString();
+        if (user != null) {
 
-        // TODO: IMPLEMENT
+            ETuser = findViewById(R.id.up_ins_email);
+            ETpass = findViewById(R.id.up_ins_password);
+            ETconf_pass = findViewById(R.id.up_password_confirmation);
+
+            String username = ETuser.getText().toString();
+
+            if (!username.equals("")) {
+                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(username)
+                        .build();
+
+                user.updateProfile(profileUpdates)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                }
+                            }
+                        });
+            }
+
+            String pass = ETpass.getText().toString();
+            String conf_pass = ETconf_pass.getText().toString();
+
+            if (!pass.equals("") && pass.equals(conf_pass)) {
+                user.updatePassword(pass)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                }
+                            }
+                        });
+            }
+        }
     }
 
     public void editUserImage(View view) {
